71e3b56dd403d88d2f59769693389fb24c378aff
shreyyyyy <jain.shreyans0601@gmail.com>
2024-09-29 23:28:12 +0530

    Items.jsx

--- app/_components/Items.jsx
+++ app/_components/Items.jsx
@@ -8,10 +8,8 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import ItemDetails from './ItemDetails';
-
-
 
 function Items({ items }) {
     return (
@@ -20,17 +18,16 @@ function Items({ items }) {
             gap-3 border rounded-lg 
             transition-transform transform hover:scale-105 
             hover:shadow-xl hover:shadow-pink-600 
-            max-w-xs w-full' // Restrict max width and make it full width
+            max-w-xs w-full'
         >
-
             <Image
                 src={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}${items.images[0].url}`}
                 alt='Item Image'
-                width={200} // Setting width for a smaller size on mobile
-                height={200} // Maintain aspect ratio
-                className='w-full h-auto object-contain' // Ensure the image fits within the box while maintaining aspect ratio
+                width={200}
+                height={200}
+                className='w-full h-auto object-contain'
+                onError={(e) => { e.target.onerror = null; e.target.src='/path/to/fallback-image.jpg'; }}
             />
-
 
             <h2 className='font-semibold text-lg text-center'>
                 {items.name}
@@ -38,25 +35,26 @@ function Items({ items }) {
             <h2 className='text-xl font-bold text-green-500'>₹{items.mrp}</h2>
             <h2 className='text-xl line-through font-medium text-red-500'>
                 ₹{items.sellingprice}
-
             </h2>
 
             <Dialog>
                 <DialogTrigger asChild>
-                    <Button variant="outline" className='transform transition-transform hover:scale-110'>
+                    <Button 
+                        variant="outline" 
+                        className='transform transition-transform hover:scale-110'
+                        aria-label={`Add ${items.name} to cart`}
+                    >
                         🛒 Add to Cart
                     </Button>
                 </DialogTrigger>
                 <DialogContent>
                     <DialogHeader>
                         <DialogDescription>
-                            <ItemDetails items = {items}/>
+                            <ItemDetails items={items} />
                         </DialogDescription>
                     </DialogHeader>
                 </DialogContent>
             </Dialog>
-
-
         </div>
     );
 }

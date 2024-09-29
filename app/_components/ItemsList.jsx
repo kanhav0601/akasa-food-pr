02b4ad7ebd3457b55a89152e0cf5ae7728f504f6
shreyyyyy <jain.shreyans0601@gmail.com>
2024-09-29 23:28:44 +0530

    ItemsList.jsx

--- app/_components/ItemsList.jsx
+++ app/_components/ItemsList.jsx
@@ -6,10 +6,13 @@ function ItemsList({ itemList }) {
             <h2 className='text-center text-orange-600 font-bold text-3xl md:text-4xl lg:text-5xl'>
                 Sky-High Eats to Fuel Your Journey!
             </h2>
-            <div className='p-8 grid grid-cols-2 
+            <div 
+                className='p-8 grid grid-cols-2 
                 md:grid-cols-3 lg:grid-cols-4
-                gap-5'>
-                {itemList.map((items, index) => index<8&& (
+                gap-5'
+                role="grid" // Accessibility improvement
+            >
+                {itemList.slice(0, 8).map((items) => (
                     <Items key={items.id} items={items} /> // Ensure a unique key prop
                 ))}
             </div>

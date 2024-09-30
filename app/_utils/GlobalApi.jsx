1411bf82ee4b17868ca003966cd2961476879aed
shreyyyyy <jain.shreyans0601@gmail.com>
2024-09-30 14:18:32 +0530

    GlobalApi.jsx

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 const axiosClient = axios.create({
     baseURL: 'https://akasa-ad.onrender.com/api', // Base URL for the API
 });
-
 // API function to get categories
 const getCategory = () => axiosClient.get('/categories?populate=*');
 
